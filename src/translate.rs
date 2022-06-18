2e8a2dbc550e4e9329b3c1c6c0f7d5ae96f0153d
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-06-18 23:40:18 +0800

    update cargo clippy

--- src/translate.rs
+++ src/translate.rs
@@ -1,12 +1,9 @@
 pub fn generate_url(v: Vec<String>, source: String, target: String) -> String {
     let base_url = "https://translate.googleapis.com/translate_a/single";
     let q = v.join(" ");
-    format!(
-        "{}{}{}{}{}",
-        base_url,
-        "?client=gtx&ie=UTF-8&oe=UTF-8&dt=t",
-        format!("{}{}", "&sl=", source),
-        format!("{}{}", "&tl=", target),
-        format!("&q={}", q).to_string()
-    )
+    let url = "?client=gtx&ie=UTF-8&oe=UTF-8&dt=t";
+    let source = format!("{}{}", "&sl=", source);
+    let target = format!("{}{}", "&tl=", target);
+    let input = format!("&q={}", q);
+    format!("{}{}{}{}{}", base_url, url, source, target, input)
 }
