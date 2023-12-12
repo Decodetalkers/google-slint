3030320d25378b4f8fb165c59b4158fbb8994b10
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-12-12 16:39:49 +0800

    chore: some tidyup

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
 slint::include_modules!();
-use serde_json::{self, Value};
 use slint::SharedString;
 mod translate;
+
 fn main() {
     let ui = AppWindow::new().unwrap();
     let temp = ui.as_weak();
+
     ui.on_Translate(move || {
         let ui = temp.unwrap();
         let input = ui.get_input();
         let origin = ui.invoke_GetFrom();
         let target = ui.invoke_Translatefrom();
-        let output = translate::generate_url(&input, &origin, &target);
-        let translate = {
-            if let Some(item) = reqwest::blocking::get(output)
-                .and_then(|resp| resp.text())
-                .map(|body| serde_json::from_str::<Vec<Value>>(&body).unwrap())
-                .unwrap_or_else(|_| Vec::new())
-                .first()
-            {
-                item.as_array()
-                    .unwrap()
-                    .iter()
-                    .map(|s| s[0].as_str().unwrap())
-                    .collect::<Vec<&str>>()
-                    .join(" ")
-            } else {
-                "Error".to_string()
-            }
-        };
+        let url = translate::generate_url(&input, &origin, &target);
+        let translate = translate::get_translate(url).unwrap_or("Error".to_owned());
         ui.invoke_Update(SharedString::from(translate));
     });
+
     ui.run().unwrap();
 }
--- src/translate.rs
+++ src/translate.rs
@@ -1,6 +1,47 @@
+use reqwest::IntoUrl;
+use serde_json::Value;
 use url::Url;
 
 const BASE_URL: &str = "https://translate.googleapis.com/translate_a/single";
+
+#[derive(Debug, thiserror::Error)]
+pub enum TranslateError {
+    #[error("reqwest error")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("request Failed")]
+    ReqwestFailed,
+    #[error("Serde error")]
+    SerdeError(#[from] serde_json::Error),
+}
+
+pub fn get_translate<T: IntoUrl>(url: T) -> Result<String, TranslateError> {
+    let response = reqwest::blocking::get(url)?;
+    if !response.status().is_success() {
+        return Err(TranslateError::ReqwestFailed);
+    }
+    let body = response.text()?;
+
+    let arrays: Vec<Value> = serde_json::from_str(&body)?;
+
+    let first_element = arrays
+        .first()
+        .ok_or(TranslateError::ReqwestFailed)?
+        .as_array()
+        .ok_or(TranslateError::ReqwestFailed)?;
+
+    let mut res = Vec::new();
+
+    for data in first_element {
+        let Some(translate_v) = data.get(0) else {
+            continue;
+        };
+        let Some(translate) = translate_v.as_str() else {
+            continue;
+        };
+        res.push(translate.to_owned());
+    }
+    Ok(res.join(" "))
+}
 
 pub fn generate_url(v: &str, source: &str, target: &str) -> Url {
     let mut base_url = Url::parse(BASE_URL).unwrap();
