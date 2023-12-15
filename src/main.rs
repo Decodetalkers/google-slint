e5f1dbedc44b41c90724ff3a7c7f5692a34cee6e
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-12-15 11:42:01 +0800

    chore: use thread

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,19 @@ fn main() {
     let temp = ui.as_weak();
 
     ui.on_Translate(move || {
+        let temp2 = temp.clone();
         let ui = temp.unwrap();
         let input = ui.get_input();
         let origin = ui.invoke_GetFrom();
         let target = ui.invoke_Translatefrom();
         let url = translate::generate_url(&input, &origin, &target);
-        let translate = translate::get_translate(url).unwrap_or("Error".to_owned());
-        ui.invoke_Update(SharedString::from(translate));
+        std::thread::spawn(move || {
+            let translate = translate::get_translate(url).unwrap_or("Error".to_owned());
+            slint::invoke_from_event_loop(move || {
+                temp2.unwrap().invoke_Update(SharedString::from(translate))
+            })
+            .ok();
+        });
     });
 
     ui.run().unwrap();
